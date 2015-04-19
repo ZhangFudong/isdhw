d6102feec2ac1cda73716e8fc14fb80eb1a7a45a
ZhangFudong <1591112944@qq.com>
2015-04-19 22:49:25 +0800

    no message

---
+++ 01/main.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+        for j in range(i,10):
+            print("{0:1}*{1:1}={2:>2}" .format(i,j,i*j),end="  ")
+        print()
+for i in range(1,10):
+        for k in range(0,i-1):
+            print(end="        ")
+        for j in range(i,10):
+            print("{0:1}*{1:1}={2:>2}" .format(i,j,i*j),end="  ")
+        print()
+
